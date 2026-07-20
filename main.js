0d97f4ad2fd694a2214a095781ea79e2d4cb04ee
Mageba4life <andriesmageba107@gmail.com>
2026-07-20 21:57:50 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const { spawnSync } = require('child_process')
 const { existsSync, writeFileSync } = require('fs')
 
-const SESSION_ID = 'levanter_2001196c9435384f48bf094b390794fd96'
+const SESSION_ID = 'levanter_2101d2f63d81d440149446a9bdebc7da88'
 
 if (!existsSync('levanter')) {
   process.env.COREPACK_ENABLE=0
